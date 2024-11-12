3f83b647719cd76ec44403fb99029b971ba05676
youssef883 <79144849+youssef883@users.noreply.github.com>
2024-11-12 13:01:28 +0200

    test branch

--- lib/features/login/ui/screen/login_screen.dart
+++ lib/features/login/ui/screen/login_screen.dart
@@ -6,8 +6,6 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
       ),
     );
   }
